7e4c9e155e2190da0cabed409259f512bcde6b44
Adriano Raiano <adriano@raiano.ch>
2020-06-25 11:11:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// serve translations
+const express = require('express')
+const app = express()
+app.use('/locales', express.static('locales'))
+app.listen(8080)
+
+// i18next in action...
+const i18next = require('i18next')
+const HttpBackend = require('i18next-http-backend')
+// const HttpBackend = require('../../cjs')
+i18next.use(HttpBackend).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  preload: ['en', 'de', 'de-x-custom'],
+  ns: ['translation'],
+  defaultNS: 'translation',
+  backend: {
+    loadPath: 'http://localhost:8080/locales/{{lng}}/{{ns}}.json'
+  }
+}, (err, t) => {
+  if (err) return console.error(err)
+  console.log(t('welcome'))
+  console.log(t('welcome', { lng: 'de' }))
+  console.log(t('welcome', { lng: 'de-x-custom' }))
+
+  console.log(t('hi'))
+  console.log(t('hi', { lng: 'de' }))
+  console.log(t('hi', { lng: 'de-x-custom' }))
+})
